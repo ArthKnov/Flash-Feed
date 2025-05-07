5c8cc48c034525501b370a6cd3440d42f0f56fcf
ArthurTalk2Buy <arthurlima121213@gmail.com>
2025-05-07 19:49:32 -0300

    Inicio

---
+++ Flash-Feed/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './Components/Header';
+// import{post} from './Post';
+
+import './global.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+
+}
+
+export default App
