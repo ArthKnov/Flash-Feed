aacfdd710aab75aea18dc6e8bc0825df16ade131
Arthur Lima <arthurlima121213@gmail.com>
2025-05-08 02:10:49 -0300

    Sidebar inicio

--- Flash-Feed/src/App.jsx
+++ Flash-Feed/src/App.jsx
@@ -1,15 +1,28 @@
-import { Header } from './Components/Header';
-// import{post} from './Post';
+import { Header } from "./Components/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./Components/Sidebar";
+import styles from "./App.module.css";
+import "./global.css";
 
-import './global.css';
-
-function App() {
+export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+
+          <Sidebar />
+        
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda odio aliquam"
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   );
-
 }
 
-export default App
+export default App;
