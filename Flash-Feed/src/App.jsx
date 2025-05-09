e23515349403829e5b8eab8730834e61705b0297
Arthur Lima <arthurlima121213@gmail.com>
2025-05-09 06:41:38 -0300

    Projeto final em Js, com todas as funcionalidades

--- Flash-Feed/src/App.jsx
+++ Flash-Feed/src/App.jsx
@@ -1,8 +1,49 @@
 import { Header } from "./Components/Header";
 import { Post } from "./Components/Post";
 import { Sidebar } from "./Components/Sidebar";
+
 import styles from "./App.module.css";
+
 import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/arthknov.png",
+      name: "Arthur Lima",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/arthknov.png",
+      name: "Arthur Lima",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
 
 export function App() {
   return (
@@ -10,18 +51,21 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-
-          <Sidebar />
-        
+        <Sidebar />
         <main>
-          <Post
-            author="Arthur Lima"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda odio aliquam"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
   );
 }
-
 export default App;
